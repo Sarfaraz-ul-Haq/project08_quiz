54075e0dab8c8d215739198a6fd72bb1bcb28f6f
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-15 10:44:38 +0500

    next.js quiz change q9

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const answers = await inquirer.prompt([
     type: "list",
     choices: ["Angular", "React", "Vuejs", "Vanilla JavaScript"],
     message: chalk.green(
-      "Nextjs is a framework built on top of which library?"
+      "Next.js is a framework built on top of which library?"
     ),
   },
   {
@@ -15,20 +15,20 @@ const answers = await inquirer.prompt([
     type: "list",
     choices: [
       "Extensive built-in component library",
-      " Automatic code splitting for improved performance",
+      "Automatic code splitting for improved performance",
       "Built-in state management solution for small applications",
       "Faster development process compared to server-side rendering from scratch",
     ],
     message: chalk.green(
-      "What is one of the primary benefits of using Nextjs for building web applications?  "
+      "What is one of the primary benefits of using Next.js for building web applications?  "
     ),
   },
   {
     name: "q3",
     type: "list",
-    choices: ["next/link ", "next/script", "next/image", "next/head"],
+    choices: ["next/link", "next/script", "next/image", "next/head"],
     message: chalk.green(
-      "Which of the following Nextjs components helps optimize images in your application? "
+      "Which of the following Next.js components helps optimize images in your application? "
     ),
   },
   {
@@ -36,7 +36,7 @@ const answers = await inquirer.prompt([
     type: "list",
     choices: ["True", "False"],
     message: chalk.green(
-      "True or False: Nextjs applications can be statically generated, meaning the HTML is pre-rendered at build time for faster initial page loads"
+      "Next.js applications can be statically generated, meaning the HTML is pre-rendered at build time for faster initial page loads"
     ),
   },
   {
@@ -45,11 +45,11 @@ const answers = await inquirer.prompt([
     choices: [
       "Manage state within a component",
       "Fetch data on the server at build time for static pages",
-      "    Perform user-specific data manipulation before rendering",
-      " Handle server-side rendering logic for every request",
+      "Perform user-specific data manipulation before rendering",
+      "Handle server-side rendering logic for every request",
     ],
     message: chalk.green(
-      "What is the purpose of the getStaticProps function in Nextjs?"
+      "What is the purpose of the getStaticProps function in Nextj.s?"
     ),
   },
   {
@@ -57,12 +57,12 @@ const answers = await inquirer.prompt([
     type: "list",
     choices: [
       "Static generation pre-renders all pages, while SSR renders on each request",
-      " SSR provides faster initial load times, while static generation takes longer",
-      " Static generation only works for dynamic content, while SSR is better for static content",
+      "SSR provides faster initial load times, while static generation takes longer",
+      "Static generation only works for dynamic content, while SSR is better for static content",
       "They are the same thing with different names",
     ],
     message: chalk.green(
-      "What is the difference between static generation and server-side rendering (SSR) in Nextjs? "
+      "What is the difference between static generation and server-side rendering (SSR) in Next.js? "
     ),
   },
   {
@@ -70,12 +70,12 @@ const answers = await inquirer.prompt([
     type: "list",
     choices: [
       "Required for all Nextjs projects to ensure type safety",
-      " Improves developer experience with code completion and static typing",
-      "   Disables some features of Nextjs for better performance",
-      " Only works with specific libraries and frameworks",
+      "Improves developer experience with code completion and static typing",
+      "Disables some features of Next.js for better performance",
+      "Only works with specific libraries and frameworks",
     ],
     message: chalk.green(
-      "Which statement best describes the use of TypeScript with Nextjs?"
+      "Which statement best describes the use of TypeScript with Next.js?"
     ),
   },
   {
@@ -84,35 +84,37 @@ const answers = await inquirer.prompt([
     choices: [
       "Creating a file named 404js in the root pages directory",
       "Using a custom error handler component",
-      " Defining a route named error in the pages directory",
-      "  Modifying the _appjs file to handle 404 errors",
+      "Defining a route named error in the pages directory",
+      "Modifying the _appjs file to handle 404 errors",
     ],
     message: chalk.green(
-      "Which of the following is NOT a valid way to create a custom 404 page in Nextjs?    "
+      "Which of the following is NOT a valid way to create a custom 404 page in Next.js?    "
     ),
   },
   {
     name: "q9",
     type: "list",
     choices: [
-      "Defines the main application layout",
-      "  Fetches data for every page on the server",
-      "Used for client-side routing logic",
-      " Not required in modern Nextjs projects",
+      "Client-side Data Fetching (useEffect): Fetches data after the initial render in the browser, suitable for user-specific content updates",
+      "Static Generation (getStaticProps): Fetches data at build time, making it ideal for frequently updating blog posts",
+      "Server-side Rendering (getServerSideProps): Fetches data on the server for every request, offering the best performance for static content",
+      "Incremental Static Regeneration (ISR): Revalidates statically generated pages at regular intervals, perfect for data feeds that change occasionally",
     ],
-    message: chalk.green("What is the use case for the _appjs file in Nextjs?"),
+    message: chalk.green(
+      "Which of the following options accurately describes a data fetching strategy in Next.js and its ideal use case?"
+    ),
   },
   {
     name: "q10",
     type: "list",
     choices: [
       "Improved SEO and initial page load performance",
-      " Requires less code for simple applications",
-      " Offers a more complex development environment",
-      "  Provides a complete framework that replaces React",
+      "Requires less code for simple applications",
+      "Offers a more complex development environment",
+      "Provides a complete framework that replaces React",
     ],
     message: chalk.green(
-      "What are some advantages of using Nextjs compared to vanilla React when building web applications?"
+      "What are some advantages of using Next.js compared to vanilla React when building web applications?"
     ),
   },
 ]);
